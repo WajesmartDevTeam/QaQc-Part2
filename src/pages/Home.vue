53fcb3f35dcdd106b2a492ad12894e1c98ac1516
Yondiame <kassimbalogun16@gmail.com>
2020-12-01 10:12:16 +0100

    pulling

---
+++ src/pages/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="content">
+    <div class="md-layout">
+      <div class="info md-layout-item md-size-100">
+        <img
+          style="width:100%"
+          src="https://image.freepik.com/free-vector/welcome-lettering-yellow-origami-speech-bubble-with-triangles_1262-11380.jpg"
+          alt=""
+        />
+        <div class="float-right">
+          <button
+            @click="getStarted"
+            class="btn md-raised md-warning text-white btn-warning ml-3"
+            style="margin-bottom:10px"
+          >
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  beforeCreate: function() {
+    document.body.className.replace("home", "");
+  },
+  data() {
+    return {
+      role: ""
+    };
+  },
+
+  methods: {
+    getStarted() {
+      this.$router.push({ path: "exterior" });
+    }
+  }
+};
+</script>
---
+++ src/pages/Layout/DashboardLayout.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="wrapper" :class="{ 'nav-open': $sidebar.showSidebar }">
+    <!-- <notifications></notifications> -->
+
+    <side-bar>
+      <sidebar-link to="/audit/home">
+        <md-icon>dashboard</md-icon>
+        <p>Home</p>
+      </sidebar-link>
+      <sidebar-link to="/audit/exterior">
+        <md-icon>library_books</md-icon>
+        <p>Store Exterior</p>
+      </sidebar-link>
+      <sidebar-link to="/audit/merchandise">
+        <md-icon>library_books</md-icon>
+        <p>Merchandise</p>
+      </sidebar-link>
+      <sidebar-link to="/audit/production">
+        <md-icon>library_books</md-icon>
+        <p>Production</p>
+      </sidebar-link>
+      <sidebar-link to="/audit/checkout">
+        <md-icon>library_books</md-icon>
+        <p>Checkout</p>
+      </sidebar-link>
+      <sidebar-link to="/audit/instore_displays">
+        <md-icon>library_books</md-icon>
+        <p>In-store Displays</p>
+      </sidebar-link>
+      <sidebar-link to="/audit/warehouse">
+        <md-icon>library_books</md-icon>
+        <p>Warehouse</p>
+      </sidebar-link>
+      <sidebar-link to="/audit/staff">
+        <md-icon>library_books</md-icon>
+        <p>Staff</p>
+      </sidebar-link>
+      <sidebar-link to="/audit/opportunities">
+        <md-icon>library_books</md-icon>
+        <p>Opportunities</p>
+      </sidebar-link>
+      <sidebar-link to="/audit/competition">
+        <md-icon>library_books</md-icon>
+        <p>Competition</p>
+      </sidebar-link>
+      <sidebar-link to="/audit/general">
+        <md-icon>library_books</md-icon>
+        <p>General</p>
+      </sidebar-link>
+      <sidebar-link to="/audit/loss_prevention">
+        <md-icon>library_books</md-icon>
+        <p>Loss Prevention</p>
+      </sidebar-link>
+      <sidebar-link to="/audit/store_opening">
+        <md-icon>library_books</md-icon>
+        <p>Store Opening</p>
+      </sidebar-link>
+      <sidebar-link to="/audit/store_closing">
+        <md-icon>library_books</md-icon>
+        <p>Store Closing</p>
+      </sidebar-link>
+      <sidebar-link to="/audit/night_time">
+        <md-icon>library_books</md-icon>
+        <p>Night Time Visitation</p>
+      </sidebar-link>
+
+      <sidebar-link to="/" style="margin-bottom:50px;margin-left:18px">
+        <span style="height:25px" @click="logOut">
+          <md-icon>power_settings_new</md-icon>
+          <p>Log Out</p>
+        </span>
+      </sidebar-link>
+    </side-bar>
+
+    <div class="main-panel">
+      <top-navbar></top-navbar>
+
+      <dashboard-content> </dashboard-content>
+
+      <content-footer v-if="!$route.meta.hideFooter"></content-footer>
+    </div>
+  </div>
+</template>
+<script>
+import TopNavbar from "./TopNavbar.vue";
+import ContentFooter from "./ContentFooter.vue";
+import DashboardContent from "./Content.vue";
+import MobileMenu from "@/pages/Layout/MobileMenu.vue";
+
+export default {
+  components: {
+    TopNavbar,
+    DashboardContent,
+    ContentFooter,
+    MobileMenu
+  },
+  data() {
+    return {
+      role: ""
+    };
+  },
+  mounted() {
+    this.role = this.$store.getters.role;
+  },
+  methods: {
+    logOut() {
+      this.$store.dispatch("logout", true);
+      this.$router.push("index");
+    }
+  }
+};
+</script>
